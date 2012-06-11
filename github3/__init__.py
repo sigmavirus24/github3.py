974d052bf9671996d7da71b0a4e322e60ed7a6a6
SigmaVirus24 <graffatcolmingov@gmail.com>
2012-06-11 13:59:50 -0400

    May as well start using my real name.

--- github3/__init__.py
+++ github3/__init__.py
@@ -2,15 +2,15 @@
 github3
 =======
 
-:copyright: (c) 2012 by Sigmavirus24
+:copyright: (c) 2012 by Ian Cordasco
 :license: Modified BSD, see LICENSE for more details
 
 """
 
 __title__ = 'github3'
-__author__ = 'sigmavirus24'
+__author__ = 'Ian Cordasco'
 __license__ = 'Modified BSD'
-__copyright__ = 'Copyright 2012 Sigmavirus24'
+__copyright__ = 'Copyright 2012 Ian Cordasco'
 __version__ = '0.1 pre-alpha'
 
 from .api import login, gist, gists, create_gist
