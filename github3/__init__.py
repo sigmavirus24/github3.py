af2359c2e577aee6ec5ba320df83dff904072278
SigmaVirus24 <graffatcolmingov@gmail.com>
2012-06-13 11:22:30 -0400

    Obey PEP 386

--- github3/__init__.py
+++ github3/__init__.py
@@ -11,7 +11,7 @@ __title__ = 'github3'
 __author__ = 'Ian Cordasco'
 __license__ = 'Modified BSD'
 __copyright__ = 'Copyright 2012 Ian Cordasco'
-__version__ = '0.1 pre-alpha'
+__version__ = '0.1a'
 
 from .api import login, gist, gists, create_gist
 from .github import GitHub
