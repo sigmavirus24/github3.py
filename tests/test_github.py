73eb02d6f4c9dbd5d281aab4443a7fbcfff695bc
Ian Cordasco <graffatcolmingov@gmail.com>
2012-07-03 15:51:37 -0400

    Work on tests.

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,6 +1,16 @@
 import base
-import github3
+from github3 import GitHub
 
 class TestGitHub(base.BaseTest):
+    def setUp(self):
+        super(TestGitHub, self).setUp()
+        self.fake_auth = ('fake_user', 'fake_password')
+        self.fake_oauth = 'foobarbogusoauth'
+
     def test_login(self):
-        pass
+        g = GitHub()
+        g.login(**self.fake_auth)
+        self.failUnlessEqual(self.fake_auth, g._session.auth)
+        g.login(oauth=self.fake_oauth)
+        self.failUnlessEqual(g._session.headers['Authorization'],
+                'token ' + self.fake_oauth)
