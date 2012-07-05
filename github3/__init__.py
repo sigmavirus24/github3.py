80fadbfc8048510d6436fa357eee04a132997194
Ian Cordasco <graffatcolmingov@gmail.com>
2012-07-05 19:01:40 -0400

    Ship a new version to include the last bug fix

--- github3/__init__.py
+++ github3/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'github3'
 __author__ = 'Ian Cordasco'
 __license__ = 'Modified BSD'
 __copyright__ = 'Copyright 2012 Ian Cordasco'
-__version__ = '0.1a1'
+__version__ = '0.1a2'
 
 from .api import *
 from .github import GitHub
