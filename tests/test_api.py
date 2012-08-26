75b6df964ea750383eec6337ad2375c092df9db5
Ian Cordasco <graffatcolmingov@gmail.com>
2012-08-25 23:21:45 -0400

    Fix failing py3k test.

--- tests/test_api.py
+++ tests/test_api.py
@@ -43,7 +43,7 @@ class TestAPI(base.BaseTest):
         s = github3.markdown(
                 '# Header 1\n\nParagraph\n------\n# Header 2\n\nParagraph'
                 )
-        expect(s) == f
+        expect(str(s)) == f
 
     def test_organization(self):
         expect(github3.organization(self.gh3py)).isinstance(
