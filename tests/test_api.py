134a01efe758c1cd1fa259240730a32a83b21230
Ian Cordasco <graffatcolmingov@gmail.com>
2012-08-25 22:18:59 -0400

    Actually add API coverage. Add User coverage.
    Apparently I completely forgot to add the test_api file last night. Oh well.

---
+++ tests/test_api.py
@@ -0,0 +1,75 @@
+import base
+import github3
+from base import expect
+
+
+class TestAPI(base.BaseTest):
+    def test_gist(self):
+        expect(github3.gist(3156487)).isinstance(github3.gists.Gist)
+
+    def test_list_gists(self):
+        list_gists = github3.list_gists
+        self.expect_list_of_class(list_gists(), github3.gists.Gist)
+        self.expect_list_of_class(list_gists(self.sigm), github3.gists.Gist)
+
+    def test_list_followers(self):
+        self.expect_list_of_class(github3.list_followers(self.sigm),
+                github3.users.User)
+
+    def test_list_following(self):
+        self.expect_list_of_class(github3.list_following(self.sigm),
+                github3.users.User)
+
+    def test_list_repo_issues(self):
+        self.expect_list_of_class(github3.list_repo_issues(self.sigm,
+            self.todo, state='closed'), github3.issues.Issue)
+
+    def test_issue(self):
+        expect(github3.issue(self.sigm, self.todo, 1))
+
+    def test_list_events(self):
+        self.expect_list_of_class(github3.list_events(), github3.events.Event)
+
+    def test_markdown(self):
+        f = ['<h1>\n<a name="header-1" class="anchor" href="#header-1">',
+            '<span class="mini-icon mini-icon-link"></span>',
+            '</a>Header 1</h1>\n\n<h2>\n',
+            '<a name="paragraph" class="anchor" href="#paragraph">',
+            '<span class="mini-icon mini-icon-link"></span></a>Paragraph</h2>',
+            '\n\n<h1>\n<a name="header-2" class="anchor" href="#header-2">',
+            '<span class="mini-icon mini-icon-link"></span>',
+            '</a>Header 2</h1>\n\n<p>Paragraph</p>']
+        f = ''.join(f)
+        s = github3.markdown(
+                '# Header 1\n\nParagraph\n------\n# Header 2\n\nParagraph'
+                )
+        expect(s) == f
+
+    def test_organization(self):
+        expect(github3.organization(self.gh3py)).isinstance(
+                github3.orgs.Organization
+                )
+
+    def test_repository(self):
+        expect(github3.repository(self.sigm, self.todo)).isinstance(
+                github3.repos.Repository
+                )
+
+    def test_search_issues(self):
+        self.expect_list_of_class(github3.search_issues(
+            self.sigm, self.todo, 'closed', 'addons'
+            ), github3.legacy.LegacyIssue)
+
+    def test_search_users(self):
+        self.expect_list_of_class(github3.search_users('kenneth'),
+                github3.legacy.LegacyUser)
+
+    def test_search_email(self):
+        expect(github3.search_email('graffatcolmingov@gmail.com')).isinstance(
+                github3.legacy.LegacyUser)
+
+    def test_user(self):
+        expect(github3.user(self.sigm)).isinstance(github3.users.User)
+
+    def test_ratelimit_remaining(self):
+        expect(github3.ratelimit_remaining()) > 0
---
+++ tests/test_users.py
@@ -0,0 +1,73 @@
+import base
+import github3
+from base import expect
+from github3.users import User, Plan
+from github3.events import Event
+
+
+class TestUser(base.BaseTest):
+    def __init__(self, methodName='runTest'):
+        super(TestUser, self).__init__(methodName)
+        self.user = github3.user(self.sigm)
+
+    def test_user(self):
+        expect(self.user).isinstance(User)
+
+    def test_disk_usage(self):
+        expect(self.user.disk_usage) >= 0
+
+    def test_for_hire(self):
+        expect(self.user.for_hire).isinstance(bool)
+
+    def test_is_assignee_on(self):
+        expect(self.user.is_assignee_on(self.sigm, self.todo)).is_True()
+        expect(self.user.is_assignee_on(self.kr, 'requests')).is_False()
+
+    def test_list_events(self):
+        self.expect_list_of_class(self.user.list_events(), Event)
+
+    def test_list_followers(self):
+        self.expect_list_of_class(self.user.list_followers(), User)
+
+    def test_list_following(self):
+        self.expect_list_of_class(self.user.list_following(), User)
+
+    def test_list_received_events(self):
+        self.expect_list_of_class(self.user.list_received_events(), Event)
+
+    def test_owned_private_repos(self):
+        expect(self.user.owned_private_repos) >= 0
+
+    def test_private_gists(self):
+        expect(self.user.private_gists) >= 0
+
+    def test_plan(self):
+        if self.user.plan:
+            expect(self.user.plan).isinstance(Plan)
+
+    def test_public_gists(self):
+        expect(self.user.public_gists) >= 0
+
+    def test_total_private_repos(self):
+        expect(self.user.total_private_repos) >= 0
+
+    def test_requires_auth(self):
+        with expect.raises(github3.GitHubError):
+            self.user.add_email_addresses(['foo@example.com',
+                'graff@colmin.gov'])
+            self.user.delete_email_addresses(['foo@example.com'])
+            self.user.list_org_events(self.gh3py)
+            self.user.update()
+
+    def test_authenticated(self):
+        if not self.auth:
+            return
+        user = self._g.user()
+        expect(user.add_email_addresses(['new_email@gmail.com'])).is_True()
+        expect(user.delete_email_addresses(['new_email@gmail.com'])).is_True()
+        try:
+            ev = user.list_org_events(self.gh3py)
+            self.expect_list_of_class(ev, Event)
+        except github3.GitHubError:
+            pass
+        expect(user.update()).isinstance(bool)
