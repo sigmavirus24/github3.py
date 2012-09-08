bd97364d80996668b77c8e368a21c73a1e082f11
Ian Cordasco <graffatcolmingov@gmail.com>
2012-09-08 16:31:44 -0400

    This should complete coverage for events.

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,7 +1,12 @@
 from base import BaseTest, expect, str_test
 from github3.events import Event, _payload_handlers
 from github3.orgs import Organization
-from github3.repos import RepoComment, Download
+from github3.repos import RepoComment, Download, Repository
+from github3.users import User
+from github3.gists import Gist
+from github3.issues import Issue, IssueComment
+from github3.pulls import PullRequest, ReviewComment
+from github3.orgs import Team
 
 
 class TestEvent(BaseTest):
@@ -38,19 +43,61 @@ def handler(name):
 
 
 class TestPayloadHandlers(BaseTest):
+    empty_user = {
+            'login': '',
+            'id': '',
+            'avatar_url': '',
+            'gravatar_id': '',
+            'url': ''
+            }
+    date_str = '2011-01-26T20:00:00Z'
+    empty_repo = {
+            'owner': empty_user,
+            'created_at': date_str,
+            'updated_at': date_str,
+            }
+    empty_gist = {
+            'user': empty_user,
+            'files': {
+                'test.txt': {
+                    'size': '',
+                    'filename': 'test.txt',
+                    'raw_url': '',
+                    'content': 'foo'
+                    }
+                },
+            'created_at': date_str,
+            }
+    comment = {
+            'created_at': date_str,
+            'updated_at': date_str,
+            'user': empty_user,
+            'html_url': 'https://github.com/foo/bar/issues/1',
+            }
+    issue = {
+            'user': empty_user,
+            'created_at': date_str,
+            'updated_at': date_str,
+            'pull_request': {
+                'html_url': '',
+                'diff_url': '',
+                'updated_at': ''
+                },
+            'labels': [],
+            'html_url': 'https://github.com/foo/bar/issues/1',
+            }
+    link_dir = {'href': ''}
+
     def test_commitcomment(self):
         h = handler('CommitCommentEvent')
-        p = h({
-            'comment': {
-                'html_url': '',
+        p = {'comment': self.comment.copy()}
+        p['comment'].update({
                 'commit_id': '',
                 'line': '',
                 'path': '',
                 'pos': '',
-                'updated_at': '',
-                'user': {}
-                }
             })
+        p = h(p)
         expect(p['comment']).isinstance(RepoComment)
 
     def test_download(self):
@@ -68,3 +115,91 @@ class TestPayloadHandlers(BaseTest):
                 }
             })
         expect(p['download']).isinstance(Download)
+
+    def test_follow(self):
+        h = handler('FollowEvent')
+        p = h({'target': self.empty_user})
+        expect(p['target']).isinstance(User)
+
+    def test_forkev(self):
+        h = handler('ForkEvent')
+        p = h({'forkee': self.empty_repo})
+        expect(p['forkee']).isinstance(Repository)
+
+    def test_gist(self):
+        h = handler('GistEvent')
+        p = h({'gist': self.empty_gist})
+        expect(p['gist']).isinstance(Gist)
+
+    def test_issuecomment(self):
+        h = handler('IssueCommentEvent')
+        p = {'issue': self.issue, 'comment': self.comment.copy()}
+        p = h(p)
+        expect(p['issue']).isinstance(Issue)
+        expect(p['comment']).isinstance(IssueComment)
+
+    def test_issueevent(self):
+        h = handler('IssuesEvent')
+        p = h({'issue': self.issue})
+        expect(p['issue']).isinstance(Issue)
+
+    def test_member(self):
+        h = handler('MemberEvent')
+        p = h({'member': self.empty_user})
+        expect(p['member']).isinstance(User)
+
+    def test_pullreqev(self):
+        h = handler('PullRequestEvent')
+        p = h({
+            'pull_request': {
+                'created_at': self.date_str,
+                'updated_at': self.date_str,
+                'merged_at': self.date_str,
+                'closed_at': self.date_str,
+                'head': {
+                    'user': self.empty_user,
+                    'repo': self.empty_repo,
+                    },
+                'base': {
+                    'user': self.empty_user,
+                    'repo': self.empty_repo,
+                    },
+                'user': self.empty_user,
+                '_links': {
+                    'self': self.link_dir,
+                    'html': self.link_dir,
+                    'comments': self.link_dir,
+                    'review_comments': self.link_dir,
+                    },
+                'url': '',
+                }
+            })
+        expect(p['pull_request']).isinstance(PullRequest)
+
+    def test_pullreqcomm(self):
+        h = handler('PullRequestReviewCommentEvent')
+        p = {'comment': self.comment.copy()}
+        p['comment'].update({
+            '_links': {
+                'self': self.link_dir,
+                'html': self.link_dir,
+                'pull_request': self.link_dir
+                }
+            })
+        p = h(p)
+        expect(p['comment']).isinstance(ReviewComment)
+
+    def test_team(self):
+        h = handler('TeamAddEvent')
+        p = h({
+            'user': self.empty_user,
+            'repo': self.empty_repo,
+            'team': {
+                'url': '',
+                'name': '',
+                'id': ''
+                }
+            })
+        expect(p['user']).isinstance(User)
+        expect(p['repo']).isinstance(Repository)
+        expect(p['team']).isinstance(Team)
