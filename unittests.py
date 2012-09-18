fde1e9a54274091fa9f70775b05b137647348d8c
Ian Cordasco <graffatcolmingov@gmail.com>
2012-09-17 23:11:35 -0400

    Attempt to speed up running the tests.

--- unittests.py
+++ unittests.py
@@ -3,16 +3,20 @@
 # (http://git.io/todo.py)
 
 import unittest
-import sys
 import os
 import re
 from getpass import getpass
+from multiprocessing import Pool
 
 try:
     import readline
     readline.parse_and_bind('tab: complete')
 except ImportError:
     pass
+
+
+def load_test(test):
+    return unittest.defaultTestLoader.loadTestsFromName(test)
 
 if __name__ == "__main__":
     if not (os.environ.get('CI') or os.environ.get('TRAVIS')):
@@ -29,13 +33,16 @@ if __name__ == "__main__":
         os.environ['__USER'] = user
         os.environ['__PASS'] = pw
 
-    if sys.version_info >= (2, 7):
-        suite = unittest.defaultTestLoader.discover("tests")
-    else:
-        names = os.listdir("tests")
-        regex = re.compile("(?!_+)\w+\.py$")
-        join = '.'.join
-        names = [join(['tests', f[:-3]]) for f in names if regex.match(f)]
-        suite = unittest.defaultTestLoader.loadTestsFromNames(names)
+    pool = Pool(5)
+
+    names = os.listdir("tests")
+    regex = re.compile("(?!_+)\w+\.py$")
+    join = '.'.join
+    names = [join(['tests', f[:-3]]) for f in names if regex.match(f)]
+    result = pool.map_async(load_test, names)
+    suites = result.get()
+    suite = suites.pop(0)
+    for s in suites:
+        suite.addTests(s._tests)
 
     unittest.TextTestRunner(verbosity=1).run(suite)
