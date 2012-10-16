826f7f0fa6db480f8dce5268f66142ed80073454
Ian Cordasco <graffatcolmingov@gmail.com>
2012-10-16 19:58:46 -0400

    I'm feeling far more comfortable with mock now.

--- mock_tests/test_github.py
+++ mock_tests/test_github.py
@@ -1,52 +1,62 @@
 import github3
-import requests
-from json import dumps
-from mock import patch
+from json import load
+from mock import call, patch
 from unittest import TestCase
-from expecter import expect
-from mock_tests.utils import generate_response, path
+from mock_tests.utils import generate_response, expect, patch_request, path
 
 
 class TestGitHub(TestCase):
     def setUp(self):
-        self.get_r = generate_response(open(path('authorization')))
-        self.post_r = generate_response(open(path('authorization')), 201)
-        self.g_anon = github3.GitHub()
+        self.g = github3.GitHub()
 
-    @patch.object(requests.sessions.Session, 'request')
-    def test_authorization(self, request_mock):
-        request_mock.return_value = self.get_r
-        #url = 'https://api.github.com/authorizations/10'
+    @patch_request
+    def test_authorization(self, request):
+        request.return_value = generate_response('authorization')
+        url = 'https://api.github.com/authorizations/10'
         with expect.raises(github3.GitHubError):
-            self.g_anon.authorization(10)
+            self.g.authorization(10)
 
-        self.g_anon.login('user', 'password')
-        a = self.g_anon.authorization(10)
-        assert isinstance(a, github3.auths.Authorization)
-        #self.g_auth.authorization(10)
-        #self.mock_auth.get.assert_called_with(url)
+        self.g.login('user', 'password')
+        a = self.g.authorization(10)
+        expect(a).isinstance(github3.auths.Authorization)
+        assert request.called is True
+        assert call('get', url, allow_redirects=True) in request.mock_calls
 
-    @patch.object(requests.sessions.Session, 'request')
-    def test_authorize(self, request_mock):
-        request_mock.return_value = self.post_r
-        url = 'https://api.github.com/authorizations'
+    @patch_request
+    def test_authorize(self, request):
+        request.return_value = generate_response('authorization', 201)
         scopes = ['scope1', 'scope2']
-        json_scopes = dumps(scopes)
 
-        def assertions(args):
-            assert url in args
-            assert json_scopes in args[1]
-            assert '"note": ""' in args[1]
-            assert '"note_url": ""' in args[1]
+        self.g.authorize(None, None, scopes)
+        assert request.called is False
 
-        self.g_anon.authorize(None, None, scopes)
-        #assert self.mock_anon.post.call_count == 0
+        a = self.g.authorize('user', 'password', scopes)
+        expect(a).isinstance(github3.auths.Authorization)
+        assert request.called is True
 
-        a = self.g_anon.authorize('user', 'password', scopes)
-        #calls = self.sess_mock.mock_calls
-        #expect(len(calls)) == 4
-        assert isinstance(a, github3.auths.Authorization)
+    @patch_request
+    def test_create_gist(self, request):
+        request.return_value = generate_response('gist', 201)
 
-        #self.g_auth.authorize(None, None, scopes)
-        #assert self.mock_auth.post.call_count == 1
-        #assertions(self.mock_auth.post.call_args[0])
+        g = self.g.create_gist('description', 'files')
+        expect(g).isinstance(github3.gists.Gist)
+        assert request.called is True
+
+    @patch_request
+    def test_create_issue(self, request):
+        request.return_value = generate_response('issue', 201)
+
+        i = self.g.create_issue(None, None, None)
+        assert i is None
+        assert request.called is False
+
+        i = self.g.create_issue('user', 'repo', '')
+        assert i is None
+        assert request.called is False
+
+        with patch.object(github3.GitHub, 'repository') as repo:
+            repo.return_value = github3.repos.Repository(load(path('repo')))
+            i = self.g.create_issue('user', 'repo', 'Title')
+
+        expect(i).isinstance(github3.issues.Issue)
+        assert request.called is True
--- mock_tests/utils.py
+++ mock_tests/utils.py
@@ -1,14 +1,51 @@
-from requests import Response
+import requests
+from mock import patch
 from io import BytesIO
+import expecter
 
 
-def generate_response(content, status_code=200, encoding='utf-8'):
-    r = Response()
+def generate_response(path_name, status_code=200, encoding='utf-8'):
+    r = requests.Response()
     r.status_code = status_code
     r.encoding = encoding
+    content = path(path_name)
     r.raw = BytesIO(content.read().encode())
     return r
 
 
-def path(name):
-    return 'mock_tests/json/{0}'.format(name)
+def path(name, mode='r'):
+    return open('mock_tests/json/{0}'.format(name), mode)
+
+
+def patch_request(method='request'):
+    def decorator(func):
+        return patch.object(requests.sessions.Session, method)(func)
+    return decorator
+
+
+class CustomExpecter(expecter.expect):
+    def is_not_None(self):
+        assert self._actual is not None, (
+                'Expected anything but None but got it.'
+                )
+
+    def is_None(self):
+        assert self._actual is None, (
+                'Expected None but got %s' % repr(self._actual)
+                )
+
+    def is_True(self):
+        assert self._actual is True, (
+                'Expected True but got %s' % repr(self._actual)
+                )
+
+    def is_False(self):
+        assert self._actual is False, (
+                'Expected False but got %s' % repr(self._actual)
+                )
+
+    def list_of(self, cls):
+        for actual in self._actual:
+            CustomExpecter(actual).isinstance(cls)
+
+expect = CustomExpecter
