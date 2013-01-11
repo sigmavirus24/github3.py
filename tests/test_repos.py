0768ed0a87e662c7b42a7d21184da4367577364d
Ian Cordasco <graffatcolmingov@gmail.com>
2013-01-10 23:09:03 -0500

    test_iter_branches

--- tests/test_repos.py
+++ tests/test_repos.py
@@ -504,3 +504,12 @@ class TestRepository(BaseCase):
         u = next(self.repo.iter_assignees())
         expect(u).isinstance(github3.users.User)
         self.mock_assertions()
+
+    def test_iter_branches(self):
+        self.response('branch', _iter=True)
+        self.get(self.api + 'branches')
+        self.conf = {'params': None}
+
+        b = next(self.repo.iter_branches())
+        expect(b).isinstance(github3.repos.Branch)
+        self.mock_assertions()
