0020c9e186ac43b88b7654bae1fe6da9c374a968
Ian Cordasco <graffatcolmingov@gmail.com>
2013-06-02 18:20:46 -0400

    Fix #111

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if sys.argv[-1] in ("submit", "publish"):
     os.system("python setup.py sdist upload")
     sys.exit()
 
-requires.append("requests")
+requires.append("requests >= 1.2.3")
 
 __version__ = ''
 with open('github3/__init__.py', 'r') as fd:
