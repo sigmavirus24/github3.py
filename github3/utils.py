8899cddb0ec53299d8ac273429018db2755f00ce
carlio <git@carlcrowder.com>
2013-06-12 13:25:30 +0200

    Fixing isisntance check for strings depending on python 2 or 3 version

--- github3/utils.py
+++ github3/utils.py
@@ -1,6 +1,13 @@
 from datetime import datetime
 import re
 
+try:
+    # python 2
+    _base_string_class = basestring
+except NameError:
+    # python 3: no basestring class any more, everything is a str
+    _base_string_class = str
+
 # with thanks to https://code.google.com/p/jquery-localtime/issues/detail?id=4
 ISO_8601 = re.compile("^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[0-1]|0[1-9]|[1-2][0-9])"
                       "(T(2[0-3]|[0-1][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?"
@@ -17,7 +24,7 @@ def timestamp_parameter(timestamp, allow_none=True):
     if isinstance(timestamp, datetime):
         return timestamp.isoformat()
 
-    if isinstance(timestamp, basestring):
+    if isinstance(timestamp, _base_string_class):
         if not ISO_8601.match(timestamp):
             raise ValueError("Invalid timestamp: %s is not a valid ISO-8601 formatted date" % timestamp)
         return timestamp
