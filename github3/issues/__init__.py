d8cd0fe5a7e0c99907d98941596cff18cdb814c5
carlio <git@carlcrowder.com>
2013-06-12 13:35:41 +0200

    Parsing timestamp value before setting it as a parameter to prevent None being used as a parameterin requests

--- github3/issues/__init__.py
+++ github3/issues/__init__.py
@@ -31,7 +31,8 @@ def issue_params(filter, state, labels, sort, direction, since):
     if direction in ('asc', 'desc'):
         params['direction'] = direction
 
+    since = timestamp_parameter(since)
     if since:
-        params['since'] = timestamp_parameter(since)
+        params['since'] = since
 
     return params
