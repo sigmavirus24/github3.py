e9b29d0791b9252da81891e15257deb832edae77
Ian Cordasco <graffatcolmingov@gmail.com>
2013-07-06 14:57:32 -0500

    More work on validation.
    Maybe classes aren't the worst idea after all.

--- github3/structs.py
+++ github3/structs.py
@@ -1,5 +1,8 @@
 from collections import Iterator
 from github3.models import GitHubCore, urlparse
+from requests.compat import basestring
+from datetime import datetime
+import re
 
 
 class GitHubIterator(GitHubCore, Iterator):
@@ -86,27 +89,25 @@ class GitHubIterator(GitHubCore, Iterator):
         return self.__next__()
 
 
-class ParameterValidator(object):
+class ParameterValidator(dict):
     """This class is used to validate parameters sent to methods.
 
     It will use a slightly strict validation method and be capable of being
     passed directly to requests or ``json.dumps``.
+
     """
 
     def __init__(self, params, schema):
         self.params = params
+        self.update(self.params)
         self.schema = schema
         self.validate()
-
-    def __repr__(self):
-        # This is the trick to making this compatible with ``json.dumps``
-        return repr(self.params)
 
     def validate(self):
         params_copy = self.params.copy()
         for key in params_copy:
             if key not in self.schema:
-                del self.params[key]
+                self.remove_key(key)
                 # We can not pass extra information onto GitHub
                 # If we let items pass through that we don't validate, this
                 # would be a pointless exercise
@@ -114,9 +115,96 @@ class ParameterValidator(object):
         for key, validator in self.schema.items():
             if key not in self.params:
                 continue
-            if not validator.is_valid(self.params[key]):
-                try:
-                    self.params[key] = validator.convert(self.params[key])
-                except:
-                    # If we don't know how to handle it, we shouldn't pass it
-                    del self.params[key]
+            value = self.params[key]
+            if not validator.is_valid(value):
+                self.remove_key(key)
+            else:
+                self.set_key(key, validator.convert(value))
+
+    def remove_key(self, key):
+        del(self.params[key], self[key])
+
+    def set_key(self, key, value):
+        self.params[key] = self[key] = value
+
+
+class BaseValidator(object):
+    def __init__(self, allow_none=False, sub_schema=None):
+        self.allow_none = allow_none
+        self.sub_schema = sub_schema or {}
+
+    def none(self, o):
+        if self.allow_none and o is None:
+            return True
+
+    def is_valid(self, obj):
+        raise NotImplementedError(
+            "You can not use the BaseValidator's is_vaild method"
+        )
+
+
+class DateValidator(BaseValidator):
+    # with thanks to
+    # https://code.google.com/p/jquery-localtime/issues/detail?id=4
+    ISO_8601 = re.compile(
+        "^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[0-1]|0"
+        "[1-9]|[1-2][0-9])(T(2[0-3]|[0-1][0-9]):([0-5][0-9]):([0"
+        "-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[0-1][0-9]):[0-5]["
+        "0-9])?)?$"
+    )
+
+    def is_valid(self, timestamp):
+        if self.none(timestamp):
+            return True
+
+        return (isinstance(timestamp, datetime) or
+                (isinstance(timestamp, basestring) and
+                 DateValidator.ISO_8601.match(timestamp)))
+
+    def convert(self, timestamp):
+        if timestamp is None:
+            return None
+
+        if isinstance(timestamp, datetime):
+            return timestamp.isoformat()
+
+        if isinstance(timestamp, basestring):
+            if not DateValidator.ISO_8601.match(timestamp):
+                raise ValueError(
+                    ("Invalid timestamp: %s is not a valid ISO-8601"
+                     " formatted date") % timestamp)
+            return timestamp
+
+        raise ValueError(
+            "Cannot accept type %s for timestamp" % type(timestamp))
+
+
+class StringValidator(BaseValidator):
+    def is_valid(self, string):
+        if isinstance(string, basestring):
+            return True
+
+        if not self.allow_none and string is None:
+            return False
+
+        try:
+            str(string)
+        except:
+            return False
+
+        return True
+
+    @staticmethod
+    def convert(string):
+        return string if isinstance(string, basestring) else str(string)
+
+
+class DictValidator(BaseValidator):
+    def is_valid(self, dictionary):
+        try:
+            dictionary = dict(dictionary)
+        except ValueError:
+            return False
+
+        if self.sub_schema:
+            for
