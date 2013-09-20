c08fd8b05db07d9c06b5420b7feeeff1b7ada0c5
Pasquale Musella <pasquale.musella@cern.ch>
2013-09-20 22:09:42 +0200

    Regex to extract organization and repository broken for pull requests. Fixed replacing html_url with api url.

--- github3/issues/issue.py
+++ github3/issues/issue.py
@@ -65,7 +65,7 @@ class Issue(GitHubCore):
         self.number = issue.get('number')
         #: Dictionary URLs for the pull request (if they exist)
         self.pull_request = issue.get('pull_request')
-        m = match('https://github\.com/(\S+)/(\S+)/issues/\d+', self.html_url)
+        m = match('https://api.github\.com/repos/(\S+)/(\S+)/issues/\d+', self._api)
         #: Returns ('owner', 'repository') this issue was filed on.
         self.repository = m.groups()
         #: State of the issue, e.g., open, closed
