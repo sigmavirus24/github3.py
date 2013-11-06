4578fba392fe4f2fac926428918bbdc13b9b5592
Ian Cordasco <graffatcolmingov@gmail.com>
2013-11-05 20:50:38 -0600

    Switch to a ternary

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,9 @@ credentials = [os.environ.get('GH_USER', 'foo').encode(),
 
 with betamax.Betamax.configure() as config:
     config.cassette_library_dir = 'tests/cassettes'
-    record_mode = 'once'
-    if os.environ.get('TRAVIS_GH3'):
-        record_mode = 'never'
+
+    record_mode = 'never' if os.environ.get('TRAVIS_GH3') else 'once'
+
     config.default_cassette_options['record_mode'] = record_mode
 
     config.define_cassette_placeholder(
