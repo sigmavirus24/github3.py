cdb0f4bd48e954f55f91750c9f1495a98fb3d6ab
Ian Cordasco <graffatcolmingov@gmail.com>
2013-11-15 13:20:32 -0600

    Test Release#delete

--- tests/integration/test_repos_release.py
+++ tests/integration/test_repos_release.py
@@ -2,6 +2,19 @@ from .helper import IntegrationHelper
 
 
 class TestRelease(IntegrationHelper):
+    def test_delete(self):
+        """Test the ability to delete a release"""
+        self.token_login()
+        cassette_name = self.cassette_name('delete')
+        with self.recorder.use_cassette(cassette_name):
+            repository = self.gh.repository('github3py', 'github3.py')
+            release = repository.create_release(
+                '0.8.0.pre', 'develop', '0.8.0 fake release',
+                'To be deleted'
+                )
+            assert release is not None
+            assert release.delete() is True
+
     def test_edit(self):
         """Test the ability to edit a release on a repository."""
         self.token_login()
