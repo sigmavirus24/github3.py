3fdc27aac3d025dce1562bfb6380ca21ff8474af
Ian Cordasco <graffatcolmingov@gmail.com>
2013-11-23 21:14:57 -0600

    Failing test for two factor authentication

--- tests/unit/test_github.py
+++ tests/unit/test_github.py
@@ -6,3 +6,7 @@ from .helper import UnitHelper
 class TestGitHub(UnitHelper):
     described_class = GitHub
     example_data = None
+
+    def test_two_factor_login(self):
+        self.instance.login('username', 'password',
+                            two_factor_callback=lambda *args: 'foo')
