fd6236c8793f2a74e67258005107bee794235803
Ian Cordasco <graffatcolmingov@gmail.com>
2013-12-07 15:52:29 -0600

    Update test dependencies

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ packages = [
     "github3.issues",
 ]
 
-kwargs['tests_require'] = ['mock==1.0.1', 'betamax', 'pytest']
+kwargs['tests_require'] = ['mock == 1.0.1', 'betamax >=0.1.6', 'pytest']
 if sys.version_info < (3, 0):
     kwargs['tests_require'].append('unittest2==0.5.1')
 packages.append('tests')
