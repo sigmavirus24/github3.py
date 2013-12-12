f6245a2a339df924ba063e5c5316e27b5e9bf82d
Ian Cordasco <graffatcolmingov@gmail.com>
2013-12-11 21:40:50 -0600

    Fix tests/test_structs.py

--- tests/test_structs.py
+++ tests/test_structs.py
@@ -24,7 +24,7 @@ class TestGitHubIterator(BaseCase):
     def test_nexts(self):
         self.response('user', _iter=True)
         self.get(self.api_url)
-        self.conf = {'params': None, 'headers': {}}
+        self.conf = {'params': {'per_page': 10}, 'headers': {}}
         self.j = GitHubIterator(self.num, self.api_url, github3.users.User,
                                 self.g)
         assert self.j.next().login == next(self.i).login
@@ -33,7 +33,7 @@ class TestGitHubIterator(BaseCase):
     def test_catch_etags(self):
         self.response('user', _iter=True, etag='"foobarbogus"')
         self.get(self.api_url)
-        self.conf = {'params': None, 'headers': {}}
+        self.conf = {'params': {'per_page': 10}, 'headers': {}}
 
         assert isinstance(next(self.i), github3.users.User)
         assert self.i.etag == '"foobarbogus"'
@@ -42,7 +42,7 @@ class TestGitHubIterator(BaseCase):
     def test_catch_None(self):
         self.response('', 200)
         self.get(self.api_url)
-        self.conf = {'params': None, 'headers': {}}
+        self.conf = {'params': {'per_page': 10}, 'headers': {}}
 
         self.assertRaises(StopIteration, next, self.i)
 
@@ -51,7 +51,7 @@ class TestGitHubIterator(BaseCase):
     def test_entire_while_loop(self):
         self.response('user', _iter=True)
         self.get(self.api_url)
-        self.conf = {'params': None, 'headers': {}}
+        self.conf = {'params': {'per_page': 10}, 'headers': {}}
 
         assert isinstance(next(self.i), github3.users.User)
 
@@ -62,7 +62,7 @@ class TestGitHubIterator(BaseCase):
     def test_count_reaches_0(self):
         self.response('user', _iter=True)
         self.get(self.api_url)
-        self.conf = {'params': None, 'headers': {}}
+        self.conf = {'params': {'per_page': 1}, 'headers': {}}
         self.i = GitHubIterator(1, self.api_url, github3.users.User, self.g)
 
         assert isinstance(next(self.i), github3.users.User)
