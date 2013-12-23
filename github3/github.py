2699272869e8cf7b7593ac9f5edf3a23e8ae9cbe
Barry <bdmorrison@gmail.com>
2013-12-22 21:58:03 -0700

    first pass at search_issues
    Conflicts:
    	github3/api.py
    	github3/github.py
    	tests/integration/test_github.py

--- github3/github.py
+++ github3/github.py
@@ -1017,7 +1017,7 @@ class GitHub(GitHubCore):
             options: ``asc``, ``desc``; default: ``desc``
         :param int per_page: (optional)
         :param bool text_match: (optional), if True, return matching search
-            terms. See http://git.io/4ct1eQ for more information
+            terms. See http://git.io/iRmJxg for more information
         :param int number: (optional), number of repositories to return.
             Default: -1, returns all available repositories
         :param str etag: (optional), previous ETag header value
@@ -1041,6 +1041,70 @@ class GitHub(GitHubCore):
         url = self._build_url('search', 'code')
         return SearchIterator(number, url, CodeSearchResult, self, params,
                               etag, headers)
+
+    def search_issues(self, query, sort=None, order=None, per_page=None,
+                      text_match=False, number=-1, etag=None):
+        """Find issues by state and keyword
+
+        The query can contain any combination of the following supported
+        qualifers:
+
+        - ``in`` Qualifies which fields are searched. With this qualifier you
+          can restrict the search to just the repository name, description,
+          readme, or any combination of these.
+        - ``type`` With this qualifier you can restrict the search to issues or
+          pull request only.
+        - ``author`` Finds issues created by a certain user.
+        - ``assignee`` Finds issues that are assigned to a certain user.
+        - ``mentions`` Finds issues that mention a certain user.
+        - ``commenter`` Finds issues that a certain user commented on.
+        - ``involves`` Finds issues that were either created by a certain user,
+          assigned to that user, mention that user, or were commented on by
+          that user.
+        - ``created`` or ``updated`` Filters issues based on times of
+          creation, or when they were last updated. Format: ``YYYY-MM-DD``.
+          Examples: ``created:<2011``, ``pushed:<2013-02``,
+          ``pushed:>=2013-03-06``
+        - ``user`` or ``repo`` Limits searches to a specific user or repository
+        - ``language`` Searches for issues within repositories that match a
+          certain language.
+        - ``comments`` Filters issues based on the quantity of comments.
+        - ``labels`` Filters issues based on their labels.
+
+        For more information about these qualifiers, see: http://git.io/d1oELA
+
+        :param str query: (required), a valid query as described above, e.g.,
+            ``windows label:bug``
+        :param str sort: (optional), how the results should be sorted;
+            options: ``created``, ``comments``, ``updated``;
+            default: best match
+        :param str order: (optional), the direction of the sorted results,
+            options: ``asc``, ``desc``; default: ``desc``
+        :param int per_page: (optional)
+        :param bool text_match: (optional), if True, return matching search
+          terms. See http://git.io/QLQuSQ for more information
+        :param int number: (optional), number of issues to return.
+            Default: -1, returns all available issues
+        :param str etag: (optional), previous ETag header value
+        :return: generator of :class:`Issue <github3.issues.Issue>`
+        """
+        params = {'q': query}
+        headers = {}
+
+        if sort in ('comments', 'created', 'updated'):
+            params['sort'] = sort
+
+        if order in ('asc', 'desc'):
+            params['order'] = order
+
+        if text_match:
+            headers = {
+                'Accept': 'application/vnd.github.v3.full.text-match+json'
+                }
+
+        url = self._build_url('search', 'issues')
+        return SearchIterator(number, url, Issue, self, params, etag,
+                              headers)
 
     def search_repositories(self, query, sort=None, order=None,
                             per_page=None, text_match=False, number=-1,
