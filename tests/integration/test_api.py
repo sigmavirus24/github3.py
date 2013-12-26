97f4dc8bf2f7f9df20514fe1b547df5acd082f3a
Ian Cordasco <graffatcolmingov@gmail.com>
2013-12-25 21:07:19 -0600

    Re-organize the tests alphabetically

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -7,15 +7,6 @@ class TestAPI(IntegrationHelper):
     def get_client(self):
         return github3.gh
 
-    def test_search_repositories(self):
-        """Test the ability to use the repository search endpoint"""
-        cassette_name = self.cassette_name('search_repositories',
-                                           cls='GitHub')
-        with self.recorder.use_cassette(cassette_name):
-            repos = self.gh.search_repositories('github3 language:python')
-            assert isinstance(next(repos),
-                              github3.search.RepositorySearchResult)
-
     def test_search_code(self):
         """Test the ability to use the code search endpoint"""
         cassette_name = self.cassette_name('search_code',
@@ -27,3 +18,12 @@ class TestAPI(IntegrationHelper):
                 )
             assert isinstance(next(repos),
                               github3.search.CodeSearchResult)
+
+    def test_search_repositories(self):
+        """Test the ability to use the repository search endpoint"""
+        cassette_name = self.cassette_name('search_repositories',
+                                           cls='GitHub')
+        with self.recorder.use_cassette(cassette_name):
+            repos = self.gh.search_repositories('github3 language:python')
+            assert isinstance(next(repos),
+                              github3.search.RepositorySearchResult)
