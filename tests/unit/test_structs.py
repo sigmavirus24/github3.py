511393f5fae3f621b6ef94c970cee983345a9a01
Ian Cordasco <graffatcolmingov@gmail.com>
2014-01-27 20:34:40 -0600

    Test NullObject

--- tests/unit/test_structs.py
+++ tests/unit/test_structs.py
@@ -1,7 +1,8 @@
 from .helper import UnitHelper
-from github3.structs import GitHubIterator
+from github3.structs import GitHubIterator, NullObject
 
 import mock
+import pytest
 
 
 class TestGitHubIterator(UnitHelper):
@@ -38,3 +39,51 @@ class TestGitHubIterator(UnitHelper):
         i = GitHubIterator(count, url, cls, session, headers=headers)
         assert i.headers != {}
         assert i.headers.get('Accept') == 'foo'
+
+
+class TestNullObject(UnitHelper):
+    described_class = NullObject
+
+    def create_instance_of_described_class(self):
+        return self.described_class()
+
+    def test_returns_empty_list(self):
+        assert list(self.instance) == []
+
+    def test_contains_nothing(self):
+        assert 'foo' not in self.instance
+
+    def test_returns_itself_when_called(self):
+        assert self.instance('foo', 'bar', 'bogus') is self.instance
+
+    def test_returns_empty_string(self):
+        assert str(self.instance) == ''
+
+    def test_allows_arbitrary_attributes(self):
+        assert self.instance.attr is self.instance
+
+    def test_allows_arbitrary_attributes_to_be_set(self):
+        self.instance.attr = 'new'
+        assert self.instance.attr is self.instance
+
+    def test_provides_an_api_to_check_if_it_is_null(self):
+        assert self.instance.is_null()
+
+    def test_stops_iteration(self):
+        with pytest.raises(StopIteration):
+            next(self.instance)
+
+    def test_next_raises_stops_iteration(self):
+        with pytest.raises(StopIteration):
+            self.instance.next()
+
+    def test_getitem_returns_itself(self):
+        assert self.instance['attr'] is self.instance
+
+    def test_setitem_sets_nothing(self):
+        self.instance['attr'] = 'attr'
+        assert self.instance['attr'] is self.instance
+
+    def test_turns_into_unicode(self):
+        unicode_str = b''.decode('utf-8')
+        assert unicode(self.instance) == unicode_str
