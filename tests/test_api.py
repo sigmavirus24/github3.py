4ccb0c0759cbf78130212295eca240eafef2ad68
Ian Cordasco <graffatcolmingov@gmail.com>
2014-01-27 21:41:54 -0600

    Start migrating old tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -11,53 +11,6 @@ class TestAPI(TestCase):
     def tearDown(self):
         self.mock.stop()
 
-    def test_authorize(self):
-        args = ('login', 'password', ['scope1'], 'note', 'note_url.com', '',
-                '')
-        github3.authorize(*args)
-        self.gh.authorize.assert_called_with(*args)
-
-    def test_login(self):
-        args = ('login', 'password', None, None)
-        with patch.object(github3.api.GitHub, 'login') as login:
-            g = github3.login(*args)
-            assert isinstance(g, github3.github.GitHub)
-            assert not isinstance(g, github3.github.GitHubEnterprise)
-            login.assert_called_with(*args)
-
-    def test_enterprise_login(self):
-        args = ('login', 'password', None, 'http://ghe.invalid/', None)
-        with patch.object(github3.api.GitHubEnterprise, 'login') as login:
-            g = github3.login(*args)
-            assert isinstance(g, github3.github.GitHubEnterprise)
-            login.assert_called_with('login', 'password', None, None)
-
-    def test_gist(self):
-        args = (123,)
-        github3.gist(*args)
-        self.gh.gist.assert_called_with(*args)
-
-    def test_gitignore_template(self):
-        args = ('Python',)
-        github3.gitignore_template(*args)
-        self.gh.gitignore_template.assert_called_with(*args)
-
-    def test_gitignore_templates(self):
-        github3.gitignore_templates()
-        assert self.gh.gitignore_templates.called is True
-
-    def test_iter_all_repos(self):
-        github3.iter_all_repos()
-        self.gh.iter_all_repos.assert_called_with(-1, None)
-
-    def test_iter_all_users(self):
-        github3.iter_all_users()
-        self.gh.iter_all_users.assert_called_with(-1, None)
-
-    def test_iter_events(self):
-        github3.iter_events()
-        self.gh.iter_events.assert_called_with(-1, None)
-
     def test_iter_followers(self):
         github3.iter_followers('login')
         self.gh.iter_followers.assert_called_with('login', -1, None)
@@ -65,10 +18,6 @@ class TestAPI(TestCase):
     def test_iter_following(self):
         github3.iter_following('login')
         self.gh.iter_following.assert_called_with('login', -1, None)
-
-    def test_iter_gists(self):
-        github3.iter_gists()
-        self.gh.iter_gists.assert_called_with(None, -1, None)
 
     def test_iter_repo_issues(self):
         args = ('owner', 'repository', None, None, None, None, None, None,
--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -12,6 +12,24 @@ class TestAPI(unittest.TestCase):
     def tearDown(self):
         self.mocked_github.stop()
 
+    def test_all_events(self):
+        github3.all_events()
+        self.gh.iter_events.assert_called_once_with(-1, None)
+
+    def test_all_gists(self):
+        github3.all_gists()
+        self.gh.iter_gists.assert_called_once_with(None, -1, None)
+
+    def test_all_repos(self):
+        github3.all_repos()
+        # TODO(Ian): When you fix GitHub, fix this test too
+        self.gh.iter_all_repos.assert_called_once_with(-1, None)
+
+    def test_all_users(self):
+        github3.all_users()
+        # TODO(Ian): Fix this when GitHub changes
+        self.gh.iter_all_users.assert_called_once_with(-1, None)
+
     def test_authorize(self):
         args = ('login',  'password', ['scope'], 'note', 'url.com', '', '')
         github3.authorize(*args)
@@ -23,6 +41,24 @@ class TestAPI(unittest.TestCase):
             g = github3.enterprise_login(*args)
             assert isinstance(g, github3.GitHubEnterprise)
             login.assert_called_once_with('login', 'password', None, None)
+
+    def test_gist(self):
+        gist_id = 123
+        github3.gist(gist_id)
+        self.gh.gist.assert_called_once_with(gist_id)
+
+    def test_gists_for(self):
+        github3.gists_for('username')
+        self.gh.iter_gists.assert_called_once_with('username', -1, None)
+
+    def test_gitignore_template(self):
+        language = 'Python'
+        github3.gitignore_template(language)
+        self.gh.gitignore_template.assert_called_once_with(language)
+
+    def test_gitignore_templates(self):
+        github3.gitignore_templates()
+        assert self.gh.gitignore_templates.called is True
 
     def test_login(self):
         args = ('login', 'password', None, None)
