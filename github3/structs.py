7b17566c3d82e9d2871ed07853355fbda01484a7
Ian Cordasco <graffatcolmingov@gmail.com>
2014-02-02 14:19:49 -0600

    Make the NullObject false-y

--- github3/structs.py
+++ github3/structs.py
@@ -141,6 +141,11 @@ class NullObject(object):
     def __init__(self, initializer=None):
         self.__dict__['initializer'] = initializer
 
+    def __bool__(self):
+        return False
+
+    __nonzero__ = __bool__
+
     def __str__(self):
         return ''
 
