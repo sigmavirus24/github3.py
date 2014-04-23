6e47e83ef59b87856f8eee90a937356d113492f0
Ian Cordasco <graffatcolmingov@gmail.com>
2014-04-22 20:31:33 -0500

    Add integration test for GitHub#iter_forks

--- tests/integration/test_gists.py
+++ tests/integration/test_gists.py
@@ -12,3 +12,12 @@ class TestGist(IntegrationHelper):
             assert gist is not None
             for commit in gist.iter_commits():
                 assert isinstance(commit, github3.gists.history.GistHistory)
+
+    def test_iter_forks(self):
+        cassette_name = self.cassette_name('forks')
+        with self.recorder.use_cassette(cassette_name,
+                                        preserve_exact_body_bytes=True):
+            gist = self.gh.gist(1834570)
+            assert gist is not None
+            for commit in gist.iter_forks():
+                assert isinstance(commit, github3.gists.gist.Gist)
