93434ee8485950cb5251733637e96756ae9309f5
Ian Cordasco <graffatcolmingov@gmail.com>
2014-05-26 16:45:45 -0500

    Add support for specifying an environment for a deployment

--- github3/repos/repo.py
+++ github3/repos/repo.py
@@ -504,7 +504,7 @@ class Repository(GitHubCore):
 
     @requires_auth
     def create_deployment(self, ref, force=False, payload='',
-                          auto_merge=False, description=''):
+                          auto_merge=False, description='', environment=None):
         """Create a deployment.
 
         :param str ref: (required), The ref to deploy. This can be a branch,
@@ -516,13 +516,17 @@ class Repository(GitHubCore):
         :param bool auto_merge: Optional parameter to merge the default branch
             into the requested deployment branch if necessary. Default: False
         :param str description: Optional short description. Default: ""
+        :param str environment: Optional name for the target deployment
+            environment (e.g., production, staging, qa). Default: "production"
         :returns: :class:`Deployment <github3.repos.deployment.Deployment>`
         """
         json = None
         if ref:
             url = self._build_url('deployments', base_url=self._api)
             data = {'ref': ref, 'force': force, 'payload': payload,
-                    'auto_merge': auto_merge, 'description': description}
+                    'auto_merge': auto_merge, 'description': description,
+                    'environment': environment}
+            self._remove_none(data)
             headers = Deployment.CUSTOM_HEADERS
             json = self._json(self._post(url, data=data, headers=headers),
                               201)
