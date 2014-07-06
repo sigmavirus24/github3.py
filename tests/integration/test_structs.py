dcdbd0e0a9959c760d7465c748f29acd1b2e353e
Ian Cordasco <graffatcolmingov@gmail.com>
2014-07-05 19:50:12 -0500

    Fix usage of iter_all_repos in integration tests

--- tests/integration/test_structs.py
+++ tests/integration/test_structs.py
@@ -5,7 +5,7 @@ class TestGitHubIterator(IntegrationHelper):
     def test_resets_etag(self):
         cassette_name = self.cassette_name('resets_etag')
         with self.recorder.use_cassette(cassette_name):
-            users_iter = self.gh.iter_all_users(number=10)
+            users_iter = self.gh.all_users(number=10)
             assert users_iter.etag is None
             next(users_iter)  # Make the request
             assert users_iter.etag is not None
