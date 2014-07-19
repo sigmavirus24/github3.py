638e316dea9dc00ac3aa538c2b08ddced30f291a
Ian Cordasco <graffatcolmingov@gmail.com>
2014-07-18 21:49:11 -0500

    Replace Team tests

--- tests/unit/test_orgs_team.py
+++ tests/unit/test_orgs_team.py
@@ -1,6 +1,9 @@
+import pytest
+
+from github3 import GitHubError
 from github3.orgs import Team
 
-from .helper import UnitIteratorHelper
+from .helper import UnitHelper, UnitIteratorHelper
 
 
 def url_for(path=''):
@@ -8,6 +11,37 @@ def url_for(path=''):
     if path:
         path = '/' + path.strip('/')
     return 'https://api.github.com/teams/10' + path
+
+
+class TestTeam(UnitHelper):
+    described_class = Team
+    example_data = {
+        'url': 'https://api.github.com/teams/10',
+        'name': 'Owners',
+        'id': 10,
+        'permission': 'admin',
+        'members_count': 3,
+        'repos_count': 10,
+        'organization': {
+            'login': 'github',
+            'id': 1,
+            'url': 'https://api.github.com/orgs/github',
+            'avatar_url': 'https://github.com/images/error/octocat_happy.gif'
+        }
+    }
+
+    def test_add_repository(self):
+        """Show that one can add a repository to an organization team."""
+        self.instance.add_repository('name-of-repo')
+
+        self.session.put.assert_called_once_with(url_for('repos/name-of-repo'))
+
+    def test_add_repository_requires_auth(self):
+        """Show that adding a repo to a team requires authentication."""
+        self.session.has_auth.return_value = False
+
+        with pytest.raises(GitHubError):
+            self.instance.add_repository('repo')
 
 
 class TestTeamIterator(UnitIteratorHelper):
