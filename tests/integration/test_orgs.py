3cc41ac16fc19ef815f1b92a27f099abecaefccb
Ian Cordasco <graffatcolmingov@gmail.com>
2014-07-22 08:11:15 -0500

    Refactor Organization integration tests

--- tests/integration/test_orgs.py
+++ tests/integration/test_orgs.py
@@ -13,18 +13,29 @@ class TestOrganization(IntegrationHelper):
 
     betamax_kwargs = {'match_requests_on': ['method', 'uri', 'json-body']}
 
+    def get_organization(self, name='github3py'):
+        """Get the organization for each test."""
+        o = self.gh.organization(name)
+        assert isinstance(o, github3.orgs.Organization)
+        return o
+
+    def get_team(self, organization, team_name='Do Not Delete'):
+        """Get the desired team."""
+        for team in organization.teams():
+            if team.name == team_name:
+                break
+        else:
+            assert False, 'Could not find team "{0}"'.format(team_name)
+
+        return team
+
     def test_add_member(self):
         """Test the ability to add a member to an organization."""
         self.basic_login()
         cassette_name = self.cassette_name('add_member')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
-            for team in o.teams():
-                if team.name == 'Do Not Delete':
-                    break
-            else:
-                assert False, 'Could not find team'
+            o = self.get_organization()
+            team = self.get_team(o)
             assert o.add_member('esacteksab', team.id) is True
 
     def test_add_repository(self):
@@ -32,15 +43,8 @@ class TestOrganization(IntegrationHelper):
         self.basic_login()
         cassette_name = self.cassette_name('add_repository')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
-
-            for team in o.teams():
-                if team.name == 'Do Not Delete':
-                    break
-            else:
-                assert False, 'Could not find team'
-
+            o = self.get_organization()
+            team = self.get_team(o)
             assert o.add_repository('github3py/urllib3', team.id) is True
 
     def test_create_repository(self):
@@ -48,9 +52,7 @@ class TestOrganization(IntegrationHelper):
         self.basic_login()
         cassette_name = self.cassette_name('create_repository')
         with self.recorder.use_cassette(cassette_name, **self.betamax_kwargs):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
-
+            o = self.get_organization()
             r = o.create_repository('test-repository', description='hi')
             assert isinstance(r, github3.repos.Repository)
             assert r.delete() is True
@@ -60,8 +62,7 @@ class TestOrganization(IntegrationHelper):
         self.basic_login()
         cassette_name = self.cassette_name('conceal_member')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
+            o = self.get_organization()
 
             # Get a public member of the organization
             public_member = next(o.public_members())
@@ -77,8 +78,7 @@ class TestOrganization(IntegrationHelper):
         self.basic_login()
         cassette_name = self.cassette_name('create_team')
         with self.recorder.use_cassette(cassette_name, **self.betamax_kwargs):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
+            o = self.get_organization()
 
             t = o.create_team('temp-team')
             assert isinstance(t, github3.orgs.Team)
@@ -89,8 +89,7 @@ class TestOrganization(IntegrationHelper):
         self.basic_login()
         cassette_name = self.cassette_name('edit')
         with self.recorder.use_cassette(cassette_name, **self.betamax_kwargs):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
+            o = self.get_organization()
 
             assert o.edit(location='Madison, WI') is True
 
@@ -98,8 +97,7 @@ class TestOrganization(IntegrationHelper):
         """Test the ability to check if a User is a member of the org."""
         cassette_name = self.cassette_name('is_member')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
+            o = self.get_organization()
 
             assert o.is_member('sigmavirus24') is True
 
@@ -107,8 +105,7 @@ class TestOrganization(IntegrationHelper):
         """Test the ability to check if a User is a public member."""
         cassette_name = self.cassette_name('is_public_member')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
+            o = self.get_organization()
 
             assert o.is_public_member('defunkt') is False
 
@@ -116,8 +113,7 @@ class TestOrganization(IntegrationHelper):
         """Test the ability to retrieve an organization's event stream."""
         cassette_name = self.cassette_name('events')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
+            o = self.get_organization()
 
             for event in o.events():
                 assert isinstance(event, github3.events.Event)
@@ -127,8 +123,7 @@ class TestOrganization(IntegrationHelper):
         self.basic_login()
         cassette_name = self.cassette_name('members')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
+            o = self.get_organization()
 
             for member in o.members():
                 assert isinstance(member, github3.users.User)
@@ -138,8 +133,7 @@ class TestOrganization(IntegrationHelper):
         self.basic_login()
         cassette_name = self.cassette_name('public_members')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
+            o = self.get_organization()
 
             for member in o.public_members():
                 assert isinstance(member, github3.users.User)
@@ -148,8 +142,7 @@ class TestOrganization(IntegrationHelper):
         """Test the ability to retrieve an organization's repositories."""
         cassette_name = self.cassette_name('repositories')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
+            o = self.get_organization()
 
             for repo in o.repositories():
                 assert isinstance(repo, github3.repos.Repository)
@@ -159,8 +152,7 @@ class TestOrganization(IntegrationHelper):
         self.basic_login()
         cassette_name = self.cassette_name('teams')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
+            o = self.get_organization()
 
             for team in o.teams():
                 assert isinstance(team, github3.orgs.Team)
@@ -170,8 +162,7 @@ class TestOrganization(IntegrationHelper):
         self.basic_login()
         cassette_name = self.cassette_name('publicize_member')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
+            o = self.get_organization()
 
             # Show that we cannot publicize someone other than the current
             # user
@@ -185,15 +176,8 @@ class TestOrganization(IntegrationHelper):
         self.basic_login()
         cassette_name = self.cassette_name('remove_member')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
-
-            # First find the team we want to use
-            for team in o.teams():
-                if team.name == 'Do Not Delete':
-                    break
-            else:
-                assert False, 'Could not find team'
+            o = self.get_organization()
+            team = self.get_team(o)
 
             # First add the user
             assert o.add_member('gh3test', team.id) is True
@@ -205,16 +189,8 @@ class TestOrganization(IntegrationHelper):
         self.basic_login()
         cassette_name = self.cassette_name('remove_repository')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
-
-            # First find the team we want to use
-            for team in o.teams():
-                if team.name == 'Do Not Delete':
-                    break
-            else:
-                assert False, 'Could not find team'
-
+            o = self.get_organization()
+            team = self.get_team(o)
             assert o.remove_repository('github3py/urllib3', team.id) is True
 
     def test_team(self):
@@ -222,8 +198,7 @@ class TestOrganization(IntegrationHelper):
         self.basic_login()
         cassette_name = self.cassette_name('team')
         with self.recorder.use_cassette(cassette_name):
-            o = self.gh.organization('github3py')
-            assert isinstance(o, github3.orgs.Organization)
+            o = self.get_organization()
 
             # Grab a team, any team
             first_team = next(o.teams())
