131cb9abd711cc71c558e5a89d5e2b8a28ae8517
Ian Cordasco <graffatcolmingov@gmail.com>
2014-07-29 22:35:09 -0500

    Add docstrings to Gist integration tests
    @esacteksab would be so proud

--- tests/integration/test_gists.py
+++ tests/integration/test_gists.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Integration tests for methods implemented on Gist."""
 from .helper import IntegrationHelper
 
 import github3
 
 
 class TestGist(IntegrationHelper):
+
+    """Gist integration tests."""
+
     def test_comments(self):
         """Show that a user can iterate over the comments on a gist."""
         cassette_name = self.cassette_name('comments')
@@ -14,6 +19,7 @@ class TestGist(IntegrationHelper):
                 assert isinstance(comment, github3.gists.comment.GistComment)
 
     def test_iter_commits(self):
+        """Show that a user can iterate over the commits in a gist."""
         cassette_name = self.cassette_name('commits')
         with self.recorder.use_cassette(cassette_name,
                                         preserve_exact_body_bytes=True):
@@ -23,6 +29,7 @@ class TestGist(IntegrationHelper):
                 assert isinstance(commit, github3.gists.history.GistHistory)
 
     def test_iter_forks(self):
+        """Show that a user can iterate over the forks of a gist."""
         cassette_name = self.cassette_name('forks')
         with self.recorder.use_cassette(cassette_name,
                                         preserve_exact_body_bytes=True):
