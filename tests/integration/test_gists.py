fda68ab69a3f7db49bd379246513a89ea4eabfe4
Ian Cordasco <graffatcolmingov@gmail.com>
2014-07-30 21:44:53 -0500

    Add integration test for GitHub#create_comment

--- tests/integration/test_gists.py
+++ tests/integration/test_gists.py
@@ -18,6 +18,20 @@ class TestGist(IntegrationHelper):
             for comment in gist.comments():
                 assert isinstance(comment, github3.gists.comment.GistComment)
 
+    def test_create_comment(self):
+        """Show that a user can comment on a gist."""
+        self.basic_login()
+        cassette_name = self.cassette_name('create_comment')
+        with self.recorder.use_cassette(cassette_name):
+            gist = self.gh.gist('f396190a0d0047be791b')
+            assert gist is not None
+            c = gist.create_comment("""```ruby
+            mac.split('').map.with_index do |v, i|
+              positions.include?(i) ? ':' + v : v
+            end
+            ```""")
+            assert isinstance(c, github3.gists.comment.GistComment)
+
     def test_commits(self):
         """Show that a user can iterate over the commits in a gist."""
         cassette_name = self.cassette_name('commits')
