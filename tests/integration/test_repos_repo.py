1086880cb7a81229a803cf0a12cdee4a7bc0b384
Ian Cordasco <graffatcolmingov@gmail.com>
2014-07-31 20:47:10 -0500

    Add integration test for Repository#branches

--- tests/integration/test_repos_repo.py
+++ tests/integration/test_repos_repo.py
@@ -17,6 +17,15 @@ class TestRepository(IntegrationHelper):
             for assignee in repository.assignees():
                 assert isinstance(assignee, github3.users.User)
 
+    def test_branches(self):
+        """Test the ability to retrieve the brances in a repository."""
+        cassette_name = self.cassette_name('branches')
+        with self.recorder.use_cassette(cassette_name):
+            repository = self.gh.repository('sigmavirus24', 'github3.py')
+            assert repository is not None
+            for branch in repository.branches():
+                assert isinstance(branch, github3.repos.branch.Branch)
+
     def test_create_empty_blob(self):
         """Test the ability to create an empty blob on a repository."""
         self.basic_login()
