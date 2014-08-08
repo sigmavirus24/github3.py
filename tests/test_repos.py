655a785ff883b6f6f9c1f36a472434ae16801478
John Barbuto <john.barbuto@venmo.com>
2014-08-07 23:35:50 -0700

    Modify test_iter_contributors for Contributor object

--- tests/test_repos.py
+++ tests/test_repos.py
@@ -625,12 +625,12 @@ class TestRepository(BaseCase):
         self.mock_assertions()
 
     def test_iter_contributors(self):
-        self.response('user', _iter=True)
+        self.response('contributor', _iter=True)
         self.get(self.api + 'contributors')
         self.conf = {'params': {'per_page': 100}}
 
-        u = next(self.repo.iter_contributors())
-        assert isinstance(u, github3.users.User)
+        c = next(self.repo.iter_contributors())
+        assert isinstance(c, repos.contributor.Contributor)
         self.mock_assertions()
 
         self.conf = {'params': {'anon': True, 'per_page': 100}}
