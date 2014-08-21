210cb22c6f2128c0148c99bf219851895b37684c
Barry Morrison <bdmorrison@gmail.com>
2014-08-20 19:52:39 -0700

    one test pass, but my new unit test fails -- I need guidance with create_instance_of_described_class

--- tests/integration/test_users.py
+++ tests/integration/test_users.py
@@ -4,9 +4,9 @@ from .helper import IntegrationHelper
 
 
 class TestUser(IntegrationHelper):
-    def test_orgs(self):
-        cassette_name = self.cassette_name('orgs')
+    def test_organizations(self):
+        cassette_name = self.cassette_name('organizations')
         with self.recorder.use_cassette(cassette_name):
             u = self.gh.user('sigmavirus24')
-            for o in u.orgs(number=25):
+            for o in u.organizations(number=25):
                 assert isinstance(o, github3.orgs.Organization)
