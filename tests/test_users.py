54c8cac244a5e7fa4c1f50ab38d4f31bacfeba4a
Ian Cordasco <graffatcolmingov@gmail.com>
2014-08-21 20:50:19 -0500

    Replace the unit test for User#starred

--- tests/test_users.py
+++ tests/test_users.py
@@ -237,14 +237,6 @@ class TestUser(BaseCase):
         next(self.user.received_events(public=True))
         self.mock_assertions()
 
-    def test_starred(self):
-        self.response('repo', 200, _iter=True)
-        self.get(self.api + '/starred')
-
-        assert isinstance(next(self.user.starred()),
-                          github3.repos.Repository)
-        self.mock_assertions()
-
     def test_subscriptions(self):
         self.response('repo', 200, _iter=True)
         self.get(self.api + '/subscriptions')
--- tests/unit/test_users.py
+++ tests/unit/test_users.py
@@ -1,18 +1,46 @@
-import pytest
-
 import github3
 
-from github3 import GitHubError
-from github3.users import User
-
-from .helper import (UnitHelper, UnitIteratorHelper, create_url_helper,
-                     create_example_data_helper)
+from .helper import (UnitIteratorHelper, create_url_helper,)
 
 url_for = create_url_helper(
-    'https://api.github.com/users/esacteksab'
+    'https://api.github.com/users/octocat'
 )
 
-get_user_example_data = create_example_data_helper('user_example_data')
+example_data = {
+    "login": "octocat",
+    "id": 1,
+    "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+    "gravatar_id": "somehexcode",
+    "url": "https://api.github.com/users/octocat",
+    "html_url": "https://github.com/octocat",
+    "followers_url": "https://api.github.com/users/octocat/followers",
+    "following_url": ("https://api.github.com/users/octocat/following"
+                      "{/other_user}"),
+    "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+    "starred_url": ("https://api.github.com/users/octocat/starred"
+                    "{/owner}{/repo}"),
+    "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+    "organizations_url": "https://api.github.com/users/octocat/orgs",
+    "repos_url": "https://api.github.com/users/octocat/repos",
+    "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+    "received_events_url": ("https://api.github.com/users/octocat/"
+                            "received_events"),
+    "type": "User",
+    "site_admin": False,
+    "name": "monalisa octocat",
+    "company": "GitHub",
+    "blog": "https://github.com/blog",
+    "location": "San Francisco",
+    "email": "octocat@github.com",
+    "hireable": False,
+    "bio": "There once was...",
+    "public_repos": 2,
+    "public_gists": 1,
+    "followers": 20,
+    "following": 0,
+    "created_at": "2008-01-14T04:33:35Z",
+    "updated_at": "2008-01-14T04:33:35Z"
+}
 
 
 class TestUserIterators(UnitIteratorHelper):
@@ -20,6 +48,7 @@ class TestUserIterators(UnitIteratorHelper):
     """Test User methods that return iterators."""
 
     described_class = github3.users.User
+    example_data = example_data.copy()
 
     def test_followers(self):
         """Test the request to retrieve follwers."""
@@ -28,6 +57,17 @@ class TestUserIterators(UnitIteratorHelper):
 
         self.session.get.assert_called_once_with(
             url_for('followers'),
+            params={'per_page': 100},
+            headers={}
+        )
+
+    def test_starred_repositories(self):
+        """Test the request to retrieve a user's starred repos."""
+        i = self.instance.starred_repositories()
+        self.get_next(i)
+
+        self.session.get.assert_called_once_with(
+            url_for('starred'),
             params={'per_page': 100},
             headers={}
         )
