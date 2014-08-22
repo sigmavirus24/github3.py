d7cc49762343829927935e21d023154f85593b63
Ian Cordasco <graffatcolmingov@gmail.com>
2014-08-21 20:29:46 -0500

    Add integration test for User#events

--- tests/integration/test_users.py
+++ tests/integration/test_users.py
@@ -8,6 +8,17 @@ class TestUser(IntegrationHelper):
 
     """Integration tests for methods on the User class."""
 
+    def test_events(self):
+        """Show that a user can retrieve a events performed by a user."""
+        cassette_name = self.cassette_name('events')
+        with self.recorder.use_cassette(cassette_name):
+            user = self.gh.user('sigmavirus24')
+            events = list(user.events(25))
+
+        assert len(events) > 0
+        for event in events:
+            assert isinstance(event, github3.events.Event)
+
     def test_organizations(self):
         """Show that a user can retrieve any user's organizations."""
         cassette_name = self.cassette_name('organizations')
