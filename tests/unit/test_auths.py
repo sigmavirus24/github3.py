ad0a19919a5186dea7016bbb2c742c22013c448e
Ian Cordasco <graffatcolmingov@gmail.com>
2014-08-25 21:09:15 -0500

    Begin testing that authentication is required

--- tests/unit/test_auths.py
+++ tests/unit/test_auths.py
@@ -1,5 +1,6 @@
 """Unit tests for the auths module."""
 import github3
+import pytest
 
 from .helper import UnitHelper, create_url_helper
 
@@ -52,3 +53,21 @@ class TestAuthorization(UnitHelper):
         self.post_called_with(url_for(''), data={
             'scopes': ['scope-one', 'scope-two', 'scope-three'],
         })
+
+
+class TestAuthorizationRequiresAuth(UnitHelper):
+
+    """Test methods that require authentication on Authorization."""
+
+    described_class = github3.auths.Authorization
+    example_data = TestAuthorization.example_data.copy()
+
+    def after_setup(self):
+        """Disable authentication on the Session."""
+        self.session.has_auth.return_value = False
+        self.session.auth = None
+
+    def test_add_scopes(self):
+        """Test that add scopes requires authentication."""
+        with pytest.raises(github3.AuthenticationFailed):
+            self.instance.add_scopes()
