a2acb3818aea4b21e67f8d6f727dce3288cde416
Ian Cordasco <graffatcolmingov@gmail.com>
2015-04-17 08:22:24 -0500

    Coerce position to integer, not to strings
    In PullRequest#create_review_comment we were coercing the `position`
    argument to a string instead of coercing it to an integer. This was
    causing a Validation Error on GitHub's end and preventing users from
    creating review comments.
    
    Closes #374

--- github3/pulls.py
+++ github3/pulls.py
@@ -233,7 +233,7 @@ class PullRequest(GitHubCore):
         """
         url = self._build_url('comments', base_url=self._api)
         data = {'body': body, 'commit_id': commit_id, 'path': path,
-                'position': str(position)}
+                'position': int(position)}
         json = self._json(self._post(url, data=data), 201)
         return ReviewComment(json, self) if json else None
 
