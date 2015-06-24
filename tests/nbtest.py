8f09623780508253165989f9f6f6e80266b44dde
Ian Cordasco <graffatcolmingov@gmail.com>
2015-06-23 22:18:39 -0500

    Use the IPython ExecutePreprocessor in tests

--- tests/nbtest.py
+++ tests/nbtest.py
@@ -2,16 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import glob
+import json
 
 from IPython.config import Config
+from IPython.nbconvert import ExecutePreprocessor
 from IPython.nbconvert import HTMLExporter
+from IPython.nbformat import NotebookNode
 
 c = Config({
-  'ExecutePreprocessor': {'enabled': True}
+    'ExecutePreprocessor': {'enabled': True}
 })
 
 exporter = HTMLExporter(config=c)
+executor = ExecutePreprocessor(config=c)
 
 for filename in glob.glob("example-notebooks/*.ipynb"):
-  print(filename)
-  exporter.from_filename(filename)
+    print(filename)
+    exporter.from_filename(filename)
+    with open(filename, 'r') as file:
+        node = NotebookNode(json.load(file))
+    executor.preprocess(node, {})
