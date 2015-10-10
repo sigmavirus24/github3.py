bc95a2c136a65eaba929c6f79030429187e176b1
Matt Chung <itsmemattchung@gmail.com>
2015-10-09 21:55:56 -0700

    Initial commit for test_notifications.py

---
+++ tests/unit/test_notifications.py
@@ -0,0 +1,31 @@
+import github3
+import datetime
+
+from .helper import (UnitHelper, create_example_data_helper, create_url_helper)
+
+get_notification_example_data = create_example_data_helper('notification_example')
+url_for = create_url_helper(
+    'https://api.github.com/notifications/threads/6169361'
+)
+
+
+class TestThread(UnitHelper):
+    """Notification unit tests."""
+
+    described_class = github3.notifications.Thread
+    example_data = get_notification_example_data()
+
+    def test_last_read_at(self):
+        json = self.instance.as_dict().copy()
+        json['last_read_at'] = '2013-12-31T23:59:59Z'
+        thread = github3.notifications.Thread(json)
+        assert isinstance(thread.last_read_at, datetime.datetime)
+
+    def test_repr(self):
+        assert repr(self.instance) == '<Thread [{0}]>'.format(
+            self.instance.subject.get('title'))
+
+    def test_delete_description(self):
+        self.instance.delete_subscription()
+
+        self.session.delete.assert_called_once_with(url_for('subscription'))
