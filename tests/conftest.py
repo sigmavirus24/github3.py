48f4df63b8b0c0af10acdc08f9a63aef9fc85db1
Bryce Boe <bbzbryce@gmail.com>
2015-11-15 10:00:34 -0800

    Enable prettyjson for new cassettes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,13 @@ import betamax
 import os
 
 from betamax_matchers import json_body
+from betamax_serializers import pretty_json
 
 credentials = [os.environ.get('GH_USER', 'foo').encode(),
                os.environ.get('GH_PASSWORD', 'bar').encode()]
 
 betamax.Betamax.register_request_matcher(json_body.JSONBodyMatcher)
+betamax.Betamax.register_serializer(pretty_json.PrettyJSONSerializer)
 
 with betamax.Betamax.configure() as config:
     config.cassette_library_dir = 'tests/cassettes'
@@ -15,6 +17,7 @@ with betamax.Betamax.configure() as config:
     record_mode = 'never' if os.environ.get('TRAVIS_GH3') else 'once'
 
     config.default_cassette_options['record_mode'] = record_mode
+    config.default_cassette_options['serialize_with'] = 'prettyjson'
 
     config.define_cassette_placeholder(
         '<AUTH_TOKEN>',
