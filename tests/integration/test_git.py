8b56321f8e876eb2b41df907ee14c1ab2d22d9d7
Matt Chung <itsmemattchung@gmail.com>
2015-11-22 17:00:57 +0000

    Deleted tests/test_git.py after creating integration test for creating a reference.

--- tests/integration/test_git.py
+++ tests/integration/test_git.py
@@ -1,4 +1,4 @@
-"""Integration tests for Tree."""
+"""Integration tests for Git."""
 import github3
 from .helper import IntegrationHelper
 
@@ -15,3 +15,17 @@ class TestTree(IntegrationHelper):
             t = repository.tree('75b347329e3fc87ac78895ca1be58daff78872a1').recurse()
             assert isinstance(t.tree[0], github3.git.Hash)
             assert repr(t.tree[0]).startswith('<Hash')
+
+
+class TestReference(IntegrationHelper):
+
+    """Integration tests for methods on the Reference class."""
+
+    def test_update(self):
+        """Show that user can update a reference."""
+        self.token_login()
+        cassette_name = self.cassette_name('update')
+        with self.recorder.use_cassette(cassette_name):
+            repository = self.gh.repository('itsmemattchung', 'github3.py')
+            reference = repository.ref('heads/migrate-tests/git-integration-test')
+            assert reference.update('b8bcee4db99325949c4171590b8fbcc8354d54d8') is True
