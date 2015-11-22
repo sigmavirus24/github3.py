93ba412c27c2a42fb2f62bd356cb37f291f77159
Matt Chung <itsmemattchung@gmail.com>
2015-11-22 16:26:15 +0000

    Migrated git unit tests from tests/test_git.py -> tests/unit/test_git.py
    Created additional octocat examples

--- tests/unit/test_git.py
+++ tests/unit/test_git.py
@@ -6,6 +6,12 @@ get_example_data = create_example_data_helper('tree_example')
 url_for = create_url_helper('https://api.github.com/repos/octocat/Hello-World/'
                             'trees/9fb037999f264ba9a7fc6274d15fa3ae2ab98312')
 
+delete_url_for = create_url_helper('https://api.github.com/repos/octocat/Hello-World/'
+                                   'git/refs/heads/featureA')
+
+get_commit_example_data = create_example_data_helper('commit_example')
+get_reference_example_data = create_example_data_helper('reference_example')
+
 
 class TestTree(UnitHelper):
     """Tree unit test"""
@@ -24,4 +30,52 @@ class TestTree(UnitHelper):
         self.session.get.assert_called_once_with(
             url_for(),
             params={'recursive': '1'}
+        )
+
+
+class TestCommit(UnitHelper):
+
+    "Commit unit test."""
+
+    described_class = github3.git.Commit
+    example_data = get_commit_example_data()
+
+    def test_repr(self):
+        assert repr(self.instance).startswith('<Commit')
+
+    def test_committer_as_User(self):
+        """Show that commit_as_User() returns instance of User."""
+        user = self.instance.committer_as_User()
+        assert isinstance(user, github3.users.User)
+
+    def test_author_as_User(self):
+        """Show that commit_as_Author() returns instance of User."""
+        user = self.instance.author_as_User()
+        assert isinstance(user, github3.users.User)
+
+
+class TestReference(UnitHelper):
+
+    """Reference unit test."""
+
+    described_class = github3.git.Reference
+    example_data = get_reference_example_data()
+
+    def test_delete(self):
+        self.instance.delete()
+        self.session.delete.assert_called_once_with(
+            delete_url_for()
+        )
+
+    def test_repr(self):
+        assert repr(self.instance).startswith('<Reference')
+        assert repr(self.instance.object).startswith('<Git Object')
+
+    def test_update(self):
+        """Show that a user can update the reference."""
+
+        self.instance.update('fakesha', True)
+        self.session.patch.assert_called_once_with(
+            delete_url_for(),
+            data='{"sha": "fakesha", "force": true}'
         )
