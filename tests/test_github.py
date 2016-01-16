9987d8ecd386f8949e4cbb5f1bb5343a750cc129
Matt Chung <itsmemattchung@gmail.com>
2016-01-16 23:25:10 +0000

    Migrated TestGitHubStatus to tests/{unit/integration}

--- tests/test_github.py
+++ tests/test_github.py
@@ -15,37 +15,3 @@ class TestUnsecureGitHubEnterprise(BaseCase):
 
         assert False == self.g.session.verify
         assert self.request.called
-
-
-class TestGitHubStatus(BaseCase):
-    def setUp(self):
-        super(TestGitHubStatus, self).setUp()
-        self.g = github3.GitHubStatus()
-        self.api = 'https://status.github.com/'
-
-    def test_repr(self):
-        assert repr(self.g) == '<GitHub Status>'
-
-    def test_api(self):
-        self.response('user')
-        self.get(self.api + 'api.json')
-        assert isinstance(self.g.api(), dict)
-        self.mock_assertions()
-
-    def test_status(self):
-        self.response('user')
-        self.get(self.api + 'api/status.json')
-        assert isinstance(self.g.status(), dict)
-        self.mock_assertions()
-
-    def test_last_message(self):
-        self.response('user')
-        self.get(self.api + 'api/last-message.json')
-        assert isinstance(self.g.last_message(), dict)
-        self.mock_assertions()
-
-    def test_messages(self):
-        self.response('user')
-        self.get(self.api + 'api/messages.json')
-        assert isinstance(self.g.messages(), dict)
-        self.mock_assertions()
