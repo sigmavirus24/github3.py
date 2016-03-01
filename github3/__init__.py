b8325df56147099ed0882d4293b70fc1e999705b
Matthew Krueger <matthew.l.krueger@gmail.com>
2016-03-01 07:29:03 -1000

    Updated the copyright year in both __about__.py and __init__.py to "2012 - 2016"

--- github3/__init__.py
+++ github3/__init__.py
@@ -5,7 +5,7 @@ github3
 
 See http://github3.readthedocs.org/ for documentation.
 
-:copyright: (c) 2012-2014 by Ian Cordasco
+:copyright: (c) 2012-2016 by Ian Cordasco
 :license: Modified BSD, see LICENSE for more details
 
 """
