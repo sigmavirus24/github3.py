9c35b4677baf4a2e1a394adfff0f30fd9aee34d5
ministrike3 <neil123@gmail.com>
2016-07-17 16:15:57 -0700

    Update pulls.py

--- github3/pulls.py
+++ github3/pulls.py
@@ -18,7 +18,6 @@ from .decorators import requires_auth
 from .issues import Issue
 from .issues.comment import IssueComment
 from uritemplate import URITemplate
-from .repos.repo import Repository
 
 class PullDestination(models.GitHubCore):
     
@@ -46,7 +45,8 @@ class PullDestination(models.GitHubCore):
         if dest.get('repo'):
             self._repo_name = dest['repo'].get('name')
             self._repo_owner = dest['repo']['owner'].get('login')
-            self.repository = Repository(self.repository, self)
+            from .repos.repo import Repository
+            self.repository = Repository(dest.get('repo'), self)
         self.repo = (self._repo_owner, self._repo_name)
 
     def _repr(self):
