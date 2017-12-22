5042881e91e3ca5fcfec46566893678da341bcf4
Ian Stapleton Cordasco <graffatcolmingov@gmail.com>
2017-12-22 14:28:28 -0600

    Fix up last few things post merge

--- tests/unit/test_pulls.py
+++ tests/unit/test_pulls.py
@@ -150,10 +150,10 @@ class TestPullRequest(helper.UnitHelper):
 
     def test_attributes(self):
         """Show that we extract attributes correctly."""
-        assert self.instance.merge_commit_sha == \
-            'f13731c44acf96f2e5d6f0080f54e09215e36248'
+        assert (self.instance.merge_commit_sha ==
+		'e5bd3914e2e596debea16f433f57875b5b90bcd6')
         assert not self.instance.merged
-        assert not self.instance.mergeable
+        assert self.instance.mergeable
 
 
 class TestPullRequestRequiresAuthentication(
