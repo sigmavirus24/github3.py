c82b408f2a19aaaea10a5165db3dc910e4af1ebe
CQY <qychen@pku.edu.cn>
2018-12-06 19:53:04 +0800

    Fix potential infinite recursion in __getattr__

--- src/github3/models.py
+++ src/github3/models.py
@@ -54,6 +54,8 @@ class GitHubCore(object):
 
     def __getattr__(self, attribute):
         """Proxy access to stored JSON."""
+        if attribute == '_json_data':
+            raise AttributeError(attribute)
         if attribute not in self._json_data:
             raise AttributeError(attribute)
         value = self._json_data.get(attribute)
