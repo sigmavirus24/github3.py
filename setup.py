eae3cd518ac0cfd6e41264edea7d0a9eba983df2
cclauss <cclauss@me.com>
2019-05-12 10:52:49 +0200

    setup.py: Upgrade the PyPI trove classifiers

--- setup.py
+++ setup.py
@@ -92,9 +92,9 @@ setuptools.setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
     extras_require={
